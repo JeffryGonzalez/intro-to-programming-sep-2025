de2615f6f668c2e2cf1f6813d13aff50b2882487
Jeffry Gonzalez <jeff@hypertheory.com>
2025-09-12 11:23:07 -0400

    jeff kata friday

---
+++ src/KataFridayJeff/StringCalculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        var delimeters = new List<char> { ',', '\n' };
+        if (numbers == "") { return 0; }
+        if (numbers.StartsWith("//"))
+        {
+            delimeters.Add(numbers[2]);
+            numbers = numbers[4..];
+        }
+        var results = numbers.Split(delimeters.ToArray()).Select(int.Parse);
+        if (results.Any(n => n < 0))
+        {
+            throw new NegativeNumbersNotAllowedException(string.Join(", ", results.Where(n => n < 0)));
+        }
+        return results
+        .Where(n => n <= 1000)
+        .Sum();
+    }
+}
+
+public class NegativeNumbersNotAllowedException : Exception
+{
+    public NegativeNumbersNotAllowedException(string message) : base(message) { }
+}
