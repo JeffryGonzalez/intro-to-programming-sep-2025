421e988dca958902dcefe822acc30a50f68fe1ab
Jeffry Gonzalez <jeff@hypertheory.com>
2025-09-16 12:30:18 -0400

    before lunch

--- src/Links/frontend/src/demos/component/atm-withdraw.ts
+++ src/Links/frontend/src/demos/component/atm-withdraw.ts
@@ -1,54 +1,29 @@
 import { CurrencyPipe } from '@angular/common';
-import {
-  Component,
-  ChangeDetectionStrategy,
-  signal,
-  computed,
-  inject,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { BankAccountStore } from '../services/bank-account-store';
+import { BankWithdrawAmountSelector } from '../components/bank-withdraw-amount-selector';
+import { BankWithdrawTransactionActions } from '../components/bank-withdraw-transaction-actions';
 
 @Component({
   selector: 'app-demos-atm-withdraw',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CurrencyPipe],
+  imports: [
+    CurrencyPipe,
+    BankWithdrawAmountSelector,
+    BankWithdrawTransactionActions,
+  ],
   providers: [BankAccountStore],
   template: `
     <p class="text-2xl font-bold">
       Your Current Balance is {{ store.balance() | currency }}
     </p>
 
-    <div class="flex gap-4">
-      @for (amount of store.amounts; track $index) {
-        <button
-          [disabled]="store.amountLeft() - amount < 0"
-          (click)="store.addAmount(amount)"
-          class="btn btn-success"
-        >
-          {{ amount }}
-        </button>
-      }
-    </div>
+    <app-demo-bank-withdraw-amount-selector />
 
     <div>
       <p>You want to withdraw: {{ store.plannedWithdrawal() | currency }}</p>
     </div>
-    <div>
-      @if (store.plannedWithdrawal() > 0) {
-        <button (click)="store.reset()" class="btn btn-warning">Cancel</button>
-
-        @if (store.plannedWithdrawal() >= store.minimumWithdrawalAmount()) {
-          <button class="btn btn-success">
-            Make Withdrawal of {{ store.plannedWithdrawal() | currency }}
-          </button>
-        } @else {
-          <p>
-            This bank only allows withdrawals of
-            {{ store.minimumWithdrawalAmount() | currency }} or more.
-          </p>
-        }
-      }
-    </div>
+    <app-demo-bank-withdraw-transaction-actions />
   `,
   styles: ``,
 })
